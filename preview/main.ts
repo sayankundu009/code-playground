4d46e9576713b8e0cc30fb663b4c87bc5060e7c9
Sayan Kundu <sayankundu9699@gmail.com>
2022-01-16 22:57:41 +0530

    :bug: Fixed preview module error

--- preview/main.ts
+++ preview/main.ts
@@ -1,38 +1,38 @@
-(() => {
-    const iframeElement = document.querySelector("iframe");
+const iframeElement = document.querySelector("iframe");
 
-    const channel = new BroadcastChannel("code-playground");
+const channel = new BroadcastChannel("code-playground");
 
-    channel.addEventListener("message", ({data}) => {
-        switch(data.action) {
-            case "RENDER": {
-                render(data.payload.type, data.payload.content);
-                break;
-            }
+channel.addEventListener("message", ({ data }) => {
+    switch (data.action) {
+        case "RENDER": {
+            render(data.payload.type, data.payload.content);
+            break;
         }
+    }
+});
+
+function sendMessage(action: string, payload: any = {}) {
+    channel.postMessage({
+        action,
+        payload,
     });
+}
 
-    function sendMessage(action: string, payload: any = {}) {
-        channel.postMessage({
-            action,
-            payload,
-        });
-    }
-
-    function render(type: string, html: string) {
-        switch(type) {
-            case "html": {
-                if(iframeElement) {
-                    iframeElement.srcdoc = html;
-                }
-                break;
+function render(type: string, html: string) {
+    switch (type) {
+        case "html": {
+            if (iframeElement) {
+                iframeElement.srcdoc = html;
             }
+            break;
         }
     }
+}
 
-    function setup(){
-        sendMessage("INIT");
-    }
+function setup() {
+    sendMessage("INIT");
+}
 
-    setup();
-})()
+setup();
+
+export {}
